f7540d20e9c343554c40302240ef687fa68334a5
KORYUOH <coyamasss@yahoo.co.jp>
2023-10-30 02:16:14 +0900

    digits.py if exist execute script

--- conv.bat
+++ conv.bat
@@ -3,6 +3,7 @@ setlocal
 set DIRECTORY_PATH=%~dp0
 for %%i in ("%DIRECTORY_PATH:~0,-1%") do set THIS_DIRECTORY=%%~ni
 title Convert to %THIS_DIRECTORY%
+if exist digits.py python digits.py
 python conv.py
 endlocal
 exit
