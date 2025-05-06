090b0f0c1373082911e0ed67036e3bb741d558db
KORYUOH <coyamasss@yahoo.co.jp>
2025-05-07 01:23:57 +0900

    skip jpg not rename

--- conv.py
+++ conv.py
@@ -7,61 +7,64 @@ import re
 logfilename = 'log.txt'
 
 def is_imgfile( filename: str ) -> bool:
-    checkext =  [".png" , ".jpg" , ".gif" , "jpeg" , "avif"]
-    ext = filename[-4:]
-    ext = ext.lower()
-    return  ext in checkext
+	checkext =	[".png" , ".jpg" , ".gif" , "jpeg" , "avif"]
+	ext = filename[-4:]
+	ext = ext.lower()
+	return	ext in checkext
 
 def Log(message: str):
-    logfile = open( logfilename , 'a' , encoding='utf-8' )
-    logfile.write(message)
+	logfile = open( logfilename , 'a' , encoding='utf-8' )
+	logfile.write(message)
 
 def main():
-    files = os.listdir(os.getcwd())
+	files = os.listdir(os.getcwd())
 
-    for file in files :
-        try:
-            if is_imgfile(file):
-                input_im = Image.open('./'+ file)
-                if input_im.format != "JPEG":
-                    print("convert : " + file)
-                    rgb_im = input_im.convert('RGB')
-                    savename = file
-                    if savename[-4:] != ".jpg":
-                        if(savename[-4:] == "avif"):
-                            savename = file[:-4]+"jpg"
-                        else:
-                            savename = file[:-4]+".jpg"
-                    rgb_im.save('./'+savename,quality=90)
-                else:
-                    print("format Jpg : " + file)
-                    savename = file
-                    if (savename[-4:] == ".jpg" or savename[-4:] == "jpeg") :
-                        extention = ".jpg"
-                        if savename[-4:] == "jpeg" :
-                            extention = "jpeg"
+	for file in files :
+		try:
+			if is_imgfile(file):
+				input_im = Image.open('./'+ file)
+				if input_im.format != "JPEG":
+					print("convert : " + file)
+					rgb_im = input_im.convert('RGB')
+					savename = file
+					if savename[-4:] != ".jpg":
+						if(savename[-4:] == "avif"):
+							savename = file[:-4]+"jpg"
+						else:
+							savename = file[:-4]+".jpg"
+					rgb_im.save('./'+savename,quality=90)
+				else:
+					print("format Jpg : " + file)
+					savename = file
+					if (savename[-4:] == ".jpg" or savename[-4:] == "jpeg") :
+						extention = ".jpg"
+						if savename[-4:] == "jpeg" :
+							extention = "jpeg"
 
-                        savename = file[:-4]+extention
-                        print("rename "+file+" -> "+savename)
-                        input_im.save('./'+savename)
+						savename = file[:-4]+extention
+						if (savename != file):
+							print("rename "+file+" -> "+savename)
+							input_im.save('./'+savename)
+						else:
+							print("skip : "+file)
 
 
-            else : 
-                print(file + " is not image")
-        except Image.UnidentifiedImageError as e:
-            print(file + " is ErrorFile ---")
-            print(e)
-            Log(file + " is ErrorFile --- ")
-            Log(str(e))
-            Log("\n")
-        except Exception as e:
-            print(e)
-            Log("Error File : " + file)
-            Log("\n")
-            Log(str(e))
-            Log("\n")
-            Log("\n")
+			else : 
+				print(file + " is not image")
+		except Image.UnidentifiedImageError as e:
+			print(file + " is ErrorFile ---")
+			print(e)
+			Log(file + " is ErrorFile --- ")
+			Log(str(e))
+			Log("\n")
+		except Exception as e:
+			print(e)
+			Log("Error File : " + file)
+			Log("\n")
+			Log(str(e))
+			Log("\n")
+			Log("\n")
 
 
 if __name__ == '__main__':
-    main()
+	main()
